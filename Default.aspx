7eb98f7d1b8266d2b07852470a4a13b39c6966e6
Victor Mikhalev <v.mikhalev@pixlpark.com>
2023-08-17 15:10:56 +0700

    Add project files.

---
+++ Default.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="Home Page" Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="NetVipsCrash._Default" %>
+<form runat="server">
+<h1>NetVips crash</h1>
+<h2>Source image</h2>
+<img src="Images/source.jpg" height="500"/>
+
+<asp:Button runat="server" OnClick="OnServerClick" Text="Create thumbnail" id="butThumbnail"/>
+    
+</form>
---
+++ Default.aspx.cs
@@ -0,0 +1,36 @@
+﻿using NetVips;
+using System;
+using System.IO;
+using System.Web.UI;
+
+namespace NetVipsCrash
+{
+    public partial class _Default : Page
+    {
+        protected void OnServerClick(object sender, EventArgs e)
+        {
+            var sourceImagePath = Server.MapPath("~/images/source.jpg");
+            var targetImagePath = Server.MapPath("~/images/thumbnail.webp");
+            using (var source = System.IO.File.OpenRead(sourceImagePath))
+            {
+                using (var thumbnail = NetVips.Image.ThumbnailStream(source, 
+                           width: 500, 
+                           height: 500,  
+                           size: Enums.Size.Down, 
+                           failOn: Enums.FailOn.Error))
+                {
+                    var options = new VOption
+                    {
+                        { "strip", true },
+                        { "Q", 85 },
+                    };
+                    using (var output = File.Open(targetImagePath, FileMode.Create))
+                    {
+                        thumbnail.WriteToStream(output, ".webp", options);
+                    }
+                }
+            }
+            Response.Redirect(Request.Url.PathAndQuery);
+        }
+    }
+}
